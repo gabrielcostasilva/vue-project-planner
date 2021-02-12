52d60597442f2151de21a4f966b3f79cf6bdcf55
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-02-12 14:18:58 -0300

    - Added setup to Home View.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,70 @@
 <template>
   <div class="home">
-    <FilterNav @filterChange="currentFilter = $event" :currentFilter="currentFilter" />
-      <div v-if="projects.length">
-        <div v-for="project in filteredProjects" :key="project.id">
-          <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
-        </div>
+    <FilterNav
+      @filterChange="filterProjects($event)"
+      :currentFilter="currentFilter.value"
+    />
+    <div v-if="projects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import SingleProject from '../components/SingleProject'
-import FilterNav from '../components/FilterNav'
+import SingleProject from "../components/SingleProject";
+import FilterNav from "../components/FilterNav";
+import { computed, onMounted, ref } from "vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     SingleProject,
-    FilterNav
+    FilterNav,
   },
-  data() {
-    return {
-      projects: [],
-      currentFilter: 'all'
+  setup() {
+    const currentFilter = ref("all");
+    const projects = ref([]);
+
+    const filterProjects = (event) => {
+      currentFilter.value = event
+    }
+
+    onMounted(() => {
+      fetch("http://localhost:3000/projects")
+        .then((res) => res.json())
+        .then((data) => (projects.value = data))
+        .catch((err) => console.log(err.message));
+    });
+
+    const handleDelete = (id) => {
+      projects.value = projects.value.filter((project) => project.id !== id);
     };
-  },
-  mounted() {
-    fetch('http://localhost:3000/projects')
-      .then(res => res.json())
-      .then(data => this.projects = data)
-      .catch(err => console.log(err.message))
-  },
-  methods: {
-    handleDelete(id) {
-      this.projects = this.projects.filter(project => project.id !== id)
-    },
-    handleComplete(id) {
-      let selectedProject = this.projects.find(project => project.id === id)
 
-      selectedProject.complete = !selectedProject.complete
-    }
+    const handleComplete = (id) => {
+      let selectedProject = projects.value.find((project) => project.id === id);
+
+      selectedProject.complete = !selectedProject.complete;
+    };
+
+    const filteredProjects = computed(() => {
+      if (currentFilter.value === "completed") {
+        return projects.value.filter((project) => project.complete);
+      }
+
+      if (currentFilter.value === "ongoing") {
+        return projects.value.filter((project) => !project.complete);
+      }
+
+      return projects.value;
+    });
+
+    return { projects, currentFilter, handleDelete, handleComplete, filteredProjects, filterProjects };
   },
-  computed: {
-    filteredProjects() {
-      if (this.currentFilter === 'completed') {
-        return this.projects.filter(project => project.complete)
-      } 
-
-      if (this.currentFilter === 'ongoing') {
-        return this.projects.filter(project => !project.complete)
-      } 
-
-      return this.projects
-    }
-  }
-}
+};
 </script>
