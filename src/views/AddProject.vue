21a90f98c7d89ad74e24acaf367d688e84e8334a
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-02-12 13:35:30 -0300

    - Added setup method to AddProject View.

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -9,69 +9,74 @@
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            title: ' ',
-            details: ' '
-        }
-    },
-    methods: {
-        handleSubmit() {
-            let project = {
-                title: this.title,
-                details: this.details,
-                complete: false
-            }
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-            fetch('http://localhost:3000/projects',{
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify(project)
-            }).then(() => this.$router.push('/'))
-            .catch(err => console.log(err))
-        }
-    },
+export default {
+  setup() {
+    const title = ref("");
+    const details = ref("");
+
+    const router = useRouter();
+
+    const handleSubmit = () => {
+      let project = {
+        title: title.value,
+        details: details.value,
+        complete: false,
+      };
+
+      fetch("http://localhost:3000/projects", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(project),
+      })
+        .then(() => router.push("/"))
+        .catch((err) => console.log(err));
+    };
+
+    return { title, details, handleSubmit };
+  },
 };
 </script>
 
 <style>
 form {
-    background: white;
-    padding: 20px;
-    border-radius: 10px;
-  }
-  label {
-    display: block;
-    color: #bbb;
-    text-transform: uppercase;
-    font-size: 14px;
-    font-weight: bold;
-    letter-spacing: 1px;
-    margin: 20px 0 10px 0
-  }
-  input {
-    padding: 10px;
-    border: 0;
-    border-bottom: 1px solid #ddd;
-    width: 100%;
-    box-sizing: border-box;
-  }
-  textarea {
-    border: 1px solid #ddd;
-    padding: 10px;
-    width: 100%;
-    box-sizing: border-box;
-    height: 100px;
-  }
-  form button {
-    display: block;
-    margin: 20px auto 0;
-    background: #00ce89;
-    color: white;
-    padding: 10px;
-    border: 0;
-    border-radius: 6px;
-    font-size: 16px;
-  }
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
 </style>
